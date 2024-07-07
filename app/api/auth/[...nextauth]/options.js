a0627c3c77904f1dbe894a925ff2bbdda9936a26
KAMY-INDUS <kamyvit1234@gmail.com>
2024-07-07 14:08:53 +0530

    Update options.js

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -1,9 +1,7 @@
-import { NextAuthOptions } from "next-auth";
-import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-export const options=NextAuth({
+export const options={
     providers:[
         GoogleProvider({
             clientId: process.env.NEXT_AUTH_GOOGLE_CLIENT_ID,
@@ -13,4 +11,4 @@ export const options=NextAuth({
     ],
     
     secret:process.env.NEXTHAUTH_SECRET,
-})
+}

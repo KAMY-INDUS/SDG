ca913e32bc517668545db75555e02e47c16637cf
Yashvanthk05 <yashvanthk05@gmail.com>
2024-07-07 13:04:57 +0530

    final

--- app/api/firebase/route.js
+++ app/api/firebase/route.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_apiKey,
-  authDomain: process.env.NEXT_PUBLIC_authDomain,
-  projectId: process.env.NEXT_PUBLIC_projectId,
-  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
-  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
-  appId: process.env.NEXT_PUBLIC_appId,
-  measurementId: process.env.NEXT_PUBLIC_measurementId
+  apiKey: process.env.NEXT_apiKey,
+  authDomain: process.env.NEXT_authDomain,
+  projectId: process.env.NEXT_projectId,
+  storageBucket: process.env.NEXT_storageBucket,
+  messagingSenderId: process.env.NEXT_messagingSenderId,
+  appId: process.env.NEXT_appId,
+  measurementId: process.env.NEXT_measurementId
 };
 
 

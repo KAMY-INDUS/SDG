d91a6973bbcd417957961891bdc6767bbffc772e
KAMY-INDUS <kamyvit1234@gmail.com>
2024-07-08 08:38:06 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { FaAndroid } from "react-icons/fa6";
 
 const Footer = () => {
   return (
     <section id="footer">
         <div className="flex flex-col items-center justify-center">
+            <span><FaAndroid size={54} color={"#3DDC84"}/></span>
             <span>Copyright © EcoCart</span>
             <span>Created by Abinav & Yashvanth</span>
         </div>
